588f6c45e5c5be16f27ec9e142ffed7342e34c8f
hitesh <hitesh.ingale@mswipe.com>
2019-04-21 23:45:16 +0530

    table added

--- application/views/admin/booking/list.php
+++ application/views/admin/booking/list.php
@@ -1,22 +1,64 @@
 <!-- Page Content  -->
 
-
-            <h2>Collapsible Sidebar Using Bootstrap 4</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
             <div class="line"></div>
 
-            <h2>Lorem Ipsum Dolor</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <table id="custom_table">
+			  <tr>
+			    <th>Company</th>
+			    <th>Contact</th>
+			    <th>Country</th>
+			  </tr>
+			  <tr>
+			    <td>Alfreds Futterkiste</td>
+			    <td>Maria Anders</td>
+			    <td>Germany</td>
+			  </tr>
+			  <tr>
+			    <td>Berglunds snabbköp</td>
+			    <td>Christina Berglund</td>
+			    <td>Sweden</td>
+			  </tr>
+			  <tr>
+			    <td>Centro comercial Moctezuma</td>
+			    <td>Francisco Chang</td>
+			    <td>Mexico</td>
+			  </tr>
+			  <tr>
+			    <td>Ernst Handel</td>
+			    <td>Roland Mendel</td>
+			    <td>Austria</td>
+			  </tr>
+			  <tr>
+			    <td>Island Trading</td>
+			    <td>Helen Bennett</td>
+			    <td>UK</td>
+			  </tr>
+			  <tr>
+			    <td>Königlich Essen</td>
+			    <td>Philip Cramer</td>
+			    <td>Germany</td>
+			  </tr>
+			  <tr>
+			    <td>Laughing Bacchus Winecellars</td>
+			    <td>Yoshi Tannamuri</td>
+			    <td>Canada</td>
+			  </tr>
+			  <tr>
+			    <td>Magazzini Alimentari Riuniti</td>
+			    <td>Giovanni Rovelli</td>
+			    <td>Italy</td>
+			  </tr>
+			  <tr>
+			    <td>North/South</td>
+			    <td>Simon Crowther</td>
+			    <td>UK</td>
+			  </tr>
+			  <tr>
+			    <td>Paris spécialités</td>
+			    <td>Marie Bertrand</td>
+			    <td>France</td>
+			  </tr>
+			</table>
 
             <div class="line"></div>
-
-            <h2>Lorem Ipsum Dolor</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <div class="line"></div>
-
-            <h3>Lorem Ipsum Dolor</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         
